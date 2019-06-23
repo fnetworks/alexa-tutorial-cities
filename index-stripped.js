809e96893a5ab85c1a10d4207f5ed0fe9def27b8
Felix Solcher <felix.solcher@gmail.com>
2019-06-23 22:21:31 +0200

    Add model, make script work

--- index-stripped.js
+++ index-stripped.js
@@ -1,340 +1,236 @@
-/* eslint-disable  func-names */
-/* eslint-disable  no-console */
-/* eslint-disable  no-restricted-syntax */
+/* eslint-disable no-console */
+/* eslint-disable no-mixed-spaces-and-tabs */
 
 const Alexa = require('ask-sdk-core');
 
-/* INTENT HANDLERS */
 const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === `LaunchRequest`;
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(welcomeMessage)
-      .reprompt(helpMessage)
-      .getResponse();
-  },
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === `LaunchRequest`;
+	},
+	handle(handlerInput) {
+		return handlerInput.responseBuilder
+			.speak(welcomeMessage)
+			.reprompt(helpMessage)
+			.getResponse();
+	}
 };
 
 const QuizHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === "IntentRequest" &&
-           (request.intent.name === "QuizIntent" || request.intent.name === "AMAZON.StartOverIntent");
-  },
-  handle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const response = handlerInput.responseBuilder;
-    attributes.state = states.QUIZ;
-    attributes.counter = 0;
-    attributes.quizScore = 0;
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === "IntentRequest" &&
+			(request.intent.name === "QuizIntent" || request.intent.name === "AMAZON.StartOverIntent");
+	},
+	handle(handlerInput) {
+		const attributesManager = handlerInput.attributesManager;
+		const attributes = attributesManager.getSessionAttributes();
+		attributes.counter = 0;
+		attributes.quizScore = 0;
 
-    var question = askQuestion(handlerInput);
-    var speakOutput = startQuizMessage + question;
-    var repromptOutput = question;
+		const question = askQuestion(attributesManager);
 
-    return response.speak(speakOutput)
-                   .reprompt(repromptOutput)
-                   .getResponse();
-  },
+		return handlerInput.responseBuilder
+			.speak(startQuizMessage + question)
+			.reprompt(question)
+			.getResponse();
+	}
 };
 
-const DefinitionHandler = { // TODO remove
-  canHandle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const request = handlerInput.requestEnvelope.request;
-
-    return attributes.state !== states.QUIZ &&
-           request.type === 'IntentRequest' &&
-           request.intent.name === 'AnswerIntent';
-  },
-  handle(handlerInput) {
-    //GRABBING ALL SLOT VALUES AND RETURNING THE MATCHING DATA OBJECT.
-    const item = getItem(handlerInput.requestEnvelope.request.intent.slots);
-    const response = handlerInput.responseBuilder;
-
-    //IF THE DATA WAS FOUND
-    if (item && item[Object.getOwnPropertyNames(data[0])[0]] !== undefined) {
-      return response.speak(getSpeechDescription(item))
-              .reprompt(repromptSpeech)
-              .getResponse();
-    } else { //IF THE DATA WAS NOT FOUND
-      return response.speak(getBadAnswer(item))
-              .reprompt(getBadAnswer(item))
-              .getResponse();
-    }
-  }
-};
+function compareSlots(slots, value) {
+	return slots["Capital"].value.toString().toLowerCase() === value.toString().toLowerCase();
+}
 
 const QuizAnswerHandler = {
-  canHandle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const request = handlerInput.requestEnvelope.request;
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
 
-    return attributes.state === states.QUIZ &&
-           request.type === 'IntentRequest' &&
-           request.intent.name === 'AnswerIntent';
-  },
-  handle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const response = handlerInput.responseBuilder;
+		return request.type === 'IntentRequest' &&
+			   request.intent.name === 'AnswerIntent';
+	},
+	handle(handlerInput) {
+		const attributesManager = handlerInput.attributesManager;
+		const attributes = attributesManager.getSessionAttributes();
 
-    var speakOutput = ``;
-    var repromptOutput = ``;
-    const item = attributes.quizItem;
-    const property = attributes.quizProperty;
-    const isCorrect = compareSlots(handlerInput.requestEnvelope.request.intent.slots, item[property]);
+		const item = attributes.quizItem;
+		const isCorrect = compareSlots(handlerInput.requestEnvelope.request.intent.slots, item.Capital);
 
-    if (isCorrect) {
-      speakOutput = getSpeechCon(true);
-      attributes.quizScore += 1;
-      handlerInput.attributesManager.setSessionAttributes(attributes);
-    } else {
-      speakOutput = getSpeechCon(false);
-    }
+		let speakOutput = getSpeechCon(isCorrect);
+		if (isCorrect) {
+			attributes.quizScore += 1;
+			attributesManager.setSessionAttributes(attributes);
+		}
 
-    speakOutput += getAnswer(property, item);
-    var question = ``;
-    //IF YOUR QUESTION COUNT IS LESS THAN 10, WE NEED TO ASK ANOTHER QUESTION.
-    if (attributes.counter < 10) {
-      speakOutput += getCurrentScore(attributes.quizScore, attributes.counter);
-      question = askQuestion(handlerInput);
-      speakOutput += question;
-      repromptOutput = question;
+		speakOutput += getAnswer(item);
+		if (attributes.counter < 10) {
+			speakOutput += getCurrentScore(attributes.quizScore, attributes.counter);
+			let question = askQuestion(attributesManager);
+			speakOutput += question;
 
-      return response.speak(speakOutput)
-      .reprompt(repromptOutput)
-      .getResponse();
-    }
-    else {
-      speakOutput += getFinalScore(attributes.quizScore, attributes.counter) + exitSkillMessage;
-      return response.speak(speakOutput).getResponse();
-    }
-  },
+			return handlerInput.responseBuilder
+				.speak(speakOutput)
+				.reprompt(question)
+				.getResponse();
+		} else {
+			speakOutput += getFinalScore(attributes.quizScore, attributes.counter) + exitSkillMessage;
+			return handlerInput.responseBuilder
+				.speak(speakOutput)
+				.getResponse();
+		}
+	}
 };
 
 const RepeatHandler = {
-  canHandle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const request = handlerInput.requestEnvelope.request;
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
 
-    return attributes.state === states.QUIZ &&
-           request.type === 'IntentRequest' &&
-           request.intent.name === 'AMAZON.RepeatHandler';
-  },
-  handle(handlerInput) {
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const question = getQuestion(attributes.counter, attributes.quizproperty, attributes.quizitem);
+		return request.type === 'IntentRequest' &&
+			   request.intent.name === 'AMAZON.RepeatHandler';
+	},
+	handle(handlerInput) {
+		const attributes = handlerInput.attributesManager.getSessionAttributes();
+		const question = getQuestion(attributes.counter, attributes.quizItem);
 
-    return handlerInput.responseBuilder
-      .speak(question)
-      .reprompt(question)
-      .getResponse();
-  },
+		return handlerInput.responseBuilder
+			.speak(question)
+			.reprompt(question)
+			.getResponse();
+	}
 };
 
 const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' &&
-           request.intent.name === 'AMAZON.HelpHandler';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(helpMessage)
-      .reprompt(helpMessage)
-      .getResponse();
-  },
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === 'IntentRequest' &&
+			   request.intent.name === 'AMAZON.HelpHandler';
+	},
+	handle(handlerInput) {
+		return handlerInput.responseBuilder
+			.speak(helpMessage)
+			.reprompt(helpMessage)
+			.getResponse();
+	}
 };
 
 const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
 
-    return request.type === `IntentRequest` && (
-              request.intent.name === 'AMAZON.StopIntent' ||
-              request.intent.name === 'AMAZON.PauseIntent' ||
-              request.intent.name === 'AMAZON.CancelIntent'
-           );
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(exitSkillMessage)
-      .getResponse();
-  },
+		return request.type === `IntentRequest` && (
+			   request.intent.name === 'AMAZON.StopIntent' ||
+			   request.intent.name === 'AMAZON.PauseIntent' ||
+			   request.intent.name === 'AMAZON.CancelIntent'
+		);
+	},
+	handle(handlerInput) {
+		return handlerInput.responseBuilder
+			.speak(exitSkillMessage)
+			.getResponse();
+	}
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder.getResponse();
-  },
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+	},
+	handle(handlerInput) {
+		return handlerInput.responseBuilder.getResponse();
+	},
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${JSON.stringify(error)}`);
-    console.log(`Handler Input: ${JSON.stringify(handlerInput)}`);
+	canHandle() {
+		return true;
+	},
+	handle(handlerInput, error) {
+		console.log(`Error handled: ${JSON.stringify(error)}`);
+		console.log(`Handler Input: ${JSON.stringify(handlerInput)}`);
 
-    return handlerInput.responseBuilder
-      .speak(helpMessage)
-      .reprompt(helpMessage)
-      .getResponse();
-  },
+		return handlerInput.responseBuilder
+			.speak(helpMessage)
+			.reprompt(helpMessage)
+			.getResponse();
+	}
 };
 
-/* CONSTANTS */
 const skillBuilder = Alexa.SkillBuilders.custom();
-const speechConsCorrect = ['Booya', 'All righty', 'Bam', 'Bazinga', 'Bingo', 'Boom', 'Bravo', 'Cha Ching', 'Cheers', 'Dynomite', 'Hip hip hooray', 'Hurrah', 'Hurray', 'Huzzah', 'Oh dear.  Just kidding.  Hurray', 'Kaboom', 'Kaching', 'Oh snap', 'Phew','Righto', 'Way to go', 'Well done', 'Whee', 'Woo hoo', 'Yay', 'Wowza', 'Yowsa'];
-const speechConsWrong = ['Argh', 'Aw man', 'Blarg', 'Blast', 'Boo', 'Bummer', 'Darn', "D'oh", 'Dun dun dun', 'Eek', 'Honk', 'Le sigh', 'Mamma mia', 'Oh boy', 'Oh dear', 'Oof', 'Ouch', 'Ruh roh', 'Shucks', 'Uh oh', 'Wah wah', 'Whoops a daisy', 'Yikes'];
-const data =[
-  { StateName: 'Bayern', Capital: 'München' },
-  { StateName: 'Badem-Württemberg', Capital: 'Stuttgart' },
-  { StateName: 'Saarland', Capital: 'Saarbrücken' },
-  { StateName: 'Rheinland-Pfalz', Capital: 'Mainz' },
-  { StateName: 'Hessen', Capital: 'Wiesbaden' },
-  { StateName: 'Thüringen', Capital: 'Erfurt' },
-  { StateName: 'Sachsen', Capital: 'Dresden' },
-  { StateName: 'Sachsen-Anhalt', Capital: 'Magdeburg' },
-  { StateName: 'Brandenburg', Capital: 'Potsdam' },
-  { StateName: 'Berlin', Capital: 'Berlin' },
-  { StateName: 'Nordrhein-Westfalen', Capital: 'Düsseldorf' },
-  { StateName: 'Niedersachsen', Capital: 'Hannover' },
-  { StateName: 'Bremen', Capital: 'Bremen' },
-  { StateName: 'Schleswig-Holstein', Capital: 'Kiel' },
-  { StateName: 'Hamburg', Capital: 'Hamburg' },
-  { StateName: 'Mecklenburg-Vorpommern', Capital: 'Schwerin' }
+const speechConsCorrect = [ 'Richtig', 'Bingo', 'Bravo' ];
+const speechConsWrong = [ 'Oh oh', 'Leider nein', 'Falsch geraten' ];
+const data = [
+	{ StateName: 'Bayern', Capital: 'München' },
+	{ StateName: 'Badem-Württemberg', Capital: 'Stuttgart' },
+	{ StateName: 'Saarland', Capital: 'Saarbrücken' },
+	{ StateName: 'Rheinland-Pfalz', Capital: 'Mainz' },
+	{ StateName: 'Hessen', Capital: 'Wiesbaden' },
+	{ StateName: 'Thüringen', Capital: 'Erfurt' },
+	{ StateName: 'Sachsen', Capital: 'Dresden' },
+	{ StateName: 'Sachsen-Anhalt', Capital: 'Magdeburg' },
+	{ StateName: 'Brandenburg', Capital: 'Potsdam' },
+	{ StateName: 'Berlin', Capital: 'Berlin' },
+	{ StateName: 'Nordrhein-Westfalen', Capital: 'Düsseldorf' },
+	{ StateName: 'Niedersachsen', Capital: 'Hannover' },
+	{ StateName: 'Bremen', Capital: 'Bremen' },
+	{ StateName: 'Schleswig-Holstein', Capital: 'Kiel' },
+	{ StateName: 'Hamburg', Capital: 'Hamburg' },
+	{ StateName: 'Mecklenburg-Vorpommern', Capital: 'Schwerin' }
 ];
-
-const states = {
-  START: `_START`,
-  QUIZ: `_QUIZ`,
-};
 
 const welcomeMessage = `Willkommen beim Hauptstadt-Quiz!`;
 const startQuizMessage = `OK.  Ich werde dir jetzt ein paar Fragen stellen. `;
 const exitSkillMessage = `Danke, dass du das Hauptstadt-Quiz gespielt hast!`;
-const repromptSpeech = `Which other state or capital would you like to know about?`; // TODO remove
 const helpMessage = `Teste dein Wissen über die Hauptstädte Deutschlands!`;
 
 /* HELPER FUNCTIONS */
 
-function getBadAnswer(item) {
-  return `Tut mir leid. Ich kenne ${item} nicht. ${helpMessage}`;
-}
-
 function getCurrentScore(score, counter) {
-  return `Du hast ${score} von ${counter} Punkten. `;
+	return `Du hast ${score} von ${counter} Punkten. `;
 }
 
 function getFinalScore(score, counter) {
-  return `Deine endgültige Punktezahl ist ${score} von ${counter} Punkten. `;
+	return `Deine endgültige Punktezahl ist ${score} von ${counter} Punkten. `;
 }
 
-function getSpeechDescription(item) {
-  return `Die Hauptstadt von ${item.StateName} ist ${item.Capital}. `;
-  // return `${item.StateName} is the ${item.StatehoodOrder}th state, admitted to the Union in ${item.StatehoodYear}.  The capital of ${item.StateName} is ${item.Capital}, and the abbreviation for ${item.StateName} is <break strength='strong'/><say-as interpret-as='spell-out'>${item.Abbreviation}</say-as>.  I've added ${item.StateName} to your Alexa app.  Which other state or capital would you like to know about?`;
+function getQuestion(counter, item) {
+	return `Hier ist deine ${counter}te Frage.  Was ist die Hauptstadt von ${item.StateName}?`;
 }
 
-function formatCasing(key) {
-  return key.split(/(?=[A-Z])/).join(' ');
-}
-
-function getQuestion(counter, property, item) { // TODO use getQuestionWithoutOrdinal instead
-  return `Hier ist deine ${counter}te Frage.  Was ist die ${formatCasing(property)} von ${item.StateName}?`;
-}
-
-function getAnswer(property, item) {
-  switch (property) {
-    case 'Abbreviation':
-      return `The ${formatCasing(property)} of ${item.StateName} is <say-as interpret-as='spell-out'>${item[property]}</say-as>. `;
-    default:
-      return `The ${formatCasing(property)} of ${item.StateName} is ${item[property]}. `;
-  }
+function getAnswer(item) {
+	return `Die Hauptstadt von ${item.StateName} ist ${item.Capital}. `;
 }
 
 function getRandom(min, max) {
-  return Math.floor((Math.random() * ((max - min) + 1)) + min);
+	return Math.floor((Math.random() * ((max - min) + 1)) + min);
 }
 
-function askQuestion(handlerInput) {
-  //GENERATING THE RANDOM QUESTION FROM DATA
-  const random = getRandom(0, data.length - 1);
-  const item = data[random];
-  const propertyArray = Object.getOwnPropertyNames(item);
-  const property = propertyArray[getRandom(1, propertyArray.length - 1)];
-
-  //GET SESSION ATTRIBUTES
-  const attributes = handlerInput.attributesManager.getSessionAttributes();
-
-  //SET QUESTION DATA TO ATTRIBUTES
-  attributes.selectedItemIndex = random;
-  attributes.quizItem = item;
-  attributes.quizProperty = property;
-  attributes.counter += 1;
-
-  //SAVE ATTRIBUTES
-  handlerInput.attributesManager.setSessionAttributes(attributes);
-
-  const question = getQuestion(attributes.counter, property, item);
-  return question;
+function randomArrayEntry(array) {
+	return array[getRandom(0, array.length - 1)];
 }
 
-function compareSlots(slots, value) {
-  for (const slot in slots) {
-    if (Object.prototype.hasOwnProperty.call(slots, slot) && slots[slot].value !== undefined) {
-      if (slots[slot].value.toString().toLowerCase() === value.toString().toLowerCase()) {
-        return true;
-      }
-    }
-  }
+function askQuestion(attributesManager) {
+	const item = randomArrayEntry(data);
 
-  return false;
+	const attributes = attributesManager.getSessionAttributes();
+	attributes.quizItem = item;
+	attributes.counter += 1;
+	attributesManager.setSessionAttributes(attributes);
+
+	return getQuestion(attributes.counter, item);
 }
 
-function getItem(slots) {
-  const propertyArray = Object.getOwnPropertyNames(data[0]);
-  let slotValue;
-
-  for (const slot in slots) {
-    if (Object.prototype.hasOwnProperty.call(slots, slot) && slots[slot].value !== undefined) {
-      slotValue = slots[slot].value;
-      for (const property in propertyArray) {
-        if (Object.prototype.hasOwnProperty.call(propertyArray, property)) {
-          const item = data.filter(x => x[propertyArray[property]]
-            .toString().toLowerCase() === slots[slot].value.toString().toLowerCase());
-          if (item.length > 0) {
-            return item[0];
-          }
-        }
-      }
-    }
-  }
-  return slotValue;
-}
-
-function getSpeechCon(type) {
-  if (type) return `<say-as interpret-as='interjection'>${speechConsCorrect[getRandom(0, speechConsCorrect.length - 1)]}! </say-as><break strength='strong'/>`;
-  return `<say-as interpret-as='interjection'>${speechConsWrong[getRandom(0, speechConsWrong.length - 1)]} </say-as><break strength='strong'/>`;
+function getSpeechCon(correct) {
+	const item = randomArrayEntry(correct ? speechConsCorrect : speechConsWrong);
+	return `<say-as interpret-as='interjection'>${item}! </say-as><break strength='strong'/>`;
 }
 
 /* LAMBDA SETUP */
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    QuizHandler,
-    DefinitionHandler, // TODO remove
-    QuizAnswerHandler,
-    RepeatHandler,
-    HelpHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+	.addRequestHandlers(
+		LaunchRequestHandler,
+		QuizHandler,
+		QuizAnswerHandler,
+		RepeatHandler,
+		HelpHandler,
+		ExitHandler,
+		SessionEndedRequestHandler
+	)
+	.addErrorHandlers(ErrorHandler)
+	.lambda();
